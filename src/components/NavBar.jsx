d17259c9175c4ba38e842a99c76f28656d1b58fa
Diego Cozer <diego.cozer16@gmail.com>
2022-08-13 01:11:53 -0300

    adc curriculo

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,17 @@
 import styles from "./NavBar.module.css";
 import { FaGithub, FaLinkedin } from "react-icons/fa";
+import { FiDownloadCloud } from "react-icons/fi";
 
 export default function navBar() {
   return (
     <header className={styles.container}>
       <a href="/" className={styles.logo}>
-        <img className={styles.logo_i} src="logo.png" alt="" style={{ width: "50px" }} />
+        <img
+          className={styles.logo_i}
+          src="logo.png"
+          alt=""
+          style={{ width: "50px" }}
+        />
       </a>
       <nav className={styles.navbar}>
         <a href="#menu">Home</a>
@@ -24,6 +30,12 @@ export default function navBar() {
           target="_blank"
         >
           <FaLinkedin />
+        </a>
+        <a
+          href="https://drive.google.com/file/d/1wkk_gmpa2LVA4OUvMRv5UVpJs5sEmNMh/view"
+          target="_blank"
+        >
+          <FiDownloadCloud />
         </a>
       </nav>
     </header>
